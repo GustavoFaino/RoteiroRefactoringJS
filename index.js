9b85c3b836c9509531112aa11c4b0855598b42ce
GustavoFaino <gustavomfaino@gmail.com>
2024-05-07 09:32:05 -0300

    3. Replace Temp with Query

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ function gerarFaturaStr (fatura, pecas) {
     const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
-  
-    function calcularTotalApresentacao(apre, peca)
+
+    function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
+                          
+
+    function calcularTotalApresentacao(apre)
     {
-      switch (peca.tipo) {
+      let total = 0;
+      switch (getPeca(apre).tipo) {
         case "tragedia":
           total = 40000;
           if (apre.audiencia > 30) {
@@ -25,7 +31,7 @@ function gerarFaturaStr (fatura, pecas) {
           total += 300 * apre.audiencia;
           break;
         default:
-            throw new Error(`Peça desconhecia: ${peca.tipo}`);
+            throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
         }
       return total;
     }
@@ -36,11 +42,11 @@ function gerarFaturaStr (fatura, pecas) {
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
