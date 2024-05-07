d41f07d9a4aca098f1022b3c26fb63a0ee4082e3
GustavoFaino <gustavomfaino@gmail.com>
2024-05-07 09:56:03 -0300

    5. Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { readFileSync } = require('fs');
 function gerarFaturaStr (fatura, pecas) {
     let totalFatura = 0;
     let creditos = 0;
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
     
     
       function formatarMoeda(valor) {
@@ -57,16 +56,38 @@ function gerarFaturaStr (fatura, pecas) {
       return creditos;
     }
 
+
+    function calcularTotalFatura() {
+      let total = 0;
+  
+      for (let apre of fatura.apresentacoes) {
+        total += calcularTotalApresentacao(apre);
+      }
+  
+      return total;
+    }
+  
+    function calcularTotalCreditos() {
+      let creditos = 0;
+  
+      for (let apre of fatura.apresentacoes) {
+        creditos += calcularCredito(apre);
+      }
+  
+      return creditos;
+    }
+  
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+
     for (let apre of fatura.apresentacoes) {
-      let total = calcularTotalApresentacao(apre);
       creditos += calcularCredito(apre);
 
-      // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-      totalFatura += total;
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
     }
-    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
+
     return faturaStr;
   }
 
